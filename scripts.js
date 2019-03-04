1bc9d5ff2a6efb1bc1a146b7162e21b155ee6d5d
TimothyFell <timothy.fell846@gmail.com>
2019-03-03 18:30:04 -0700

    Add negative numbers when range goes negative

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
+var turns = 0
+
 function set_num() {
+  turns = 0
   min = $("#min").val();
   max = $("#max").val();
-  correct_number = Math.ceil(Math.random() * max);
+  // correct_number = Math.ceil(Math.random() * (Math.abs(max)); doesnt generatue negative numbers
+  correct_number = Math.ceil( Math.random() * Math.abs(max) ) - Math.abs(min);
   $("#error").hide();
   $("#response").hide();
   $("#clear").attr("disabled", "disabled");
@@ -12,17 +16,20 @@ function set_num() {
 
 function check_guess(guess) {
   if (guess < correct_number) {
+    turns += 1
     return "That is too low"
   } else if (guess > correct_number) {
+    turns += 1
     return "That is too high"
   } else {
-    current_min = $("#min").val();
-    current_max = $("#max").val();
+    turns += 1
+    var current_min = $("#min").val();
+    var current_max = $("#max").val();
     $("#min").val(parseInt(current_min) - 10);
     $("#max").val(parseInt(current_max) + 10);
     $("#guess").val("")
     set_num();
-    $("#error").html("It just got harder.<br>Minimum is 10 lower.<br>Maximum is 10 higher.");
+    $("#error").html('It just got harder.<br>Minimum is 10 lower.<br>Maximum is 10 higher.');
     $("#error").show();
     return "BOOM!"
   }
@@ -47,6 +54,7 @@ function submit_guess() {
   $("#reset").attr("disabled", null);
   var guess = $("#guess").val();
   if (validate_guess(guess)) {
+    // switch statements and conditionals
     console.log("validated");
     $("#response").show();
     $("#recent-guess").html(guess);
